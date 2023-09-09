ae008034ed37aa4e4ecb71c618bcb922e5ddcb43
satukaroliina <satu.tarvainen@live.fi>
2023-09-09 21:49:01 +0300

    first commit

---
+++ src/main/java/BackEnd/Bookstore/model/Book.java
@@ -0,0 +1,59 @@
+package BackEnd.Bookstore.model;
+
+public class Book {
+	private String title;
+	private String author;
+	private String publicationYear;
+	private String isbn;
+	private double price;
+	
+	public Book(String title, String author, String publicationYear, String isbn, double price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.publicationYear = publicationYear;
+		this.isbn = isbn;
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPublicationYear() {
+		return publicationYear;
+	}
+
+	public void setPublicationYear(String publicationYear) {
+		this.publicationYear = publicationYear;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+}
